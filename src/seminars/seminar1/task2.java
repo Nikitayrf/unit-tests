edf2fe8414365fd9a532afb4b345793b74c64f3a
nikita <samaraphone@yandex.ru>
2023-10-10 18:16:44 +0300

    add seminar-1

---
+++ src/seminars/seminar1/task2.java
@@ -0,0 +1,11 @@
+package seminars.seminar1;
+
+public class task2 {
+    public static void main(String[] args) {
+        assertConditionB();
+    }
+    public static void assertConditionB() {
+        int x = -1;
+        assert x < 0;
+    }
+}
