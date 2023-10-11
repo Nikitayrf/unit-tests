ae20334e2d0e03c0399905c463376f216acf24dd
nikita <samaraphone@yandex.ru>
2023-10-11 20:51:14 +0300

    add lecture-2

---
+++ src/test/CalculatorTest.java
@@ -0,0 +1,82 @@
+import lectures.lecture2.main.java.Calculator;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CalculatorTest {
+
+    // Тест для сложения
+    @Test
+    void evaluatesExpression() {
+        Calculator calculator = new Calculator();
+        assertThat( calculator.calculation( 2, 6, '+' ) ).isEqualTo( 8 );
+    }
+
+    // Тест для вычитания
+    @Test
+    void subsctractionExpression() {
+        Calculator calculator = new Calculator();
+        assertThat( calculator.calculation( 2, 1, '-' ) ).isEqualTo( 1 );
+    }
+
+    // Тест для умножения
+    @Test
+    void multiplicationExpression() {
+        Calculator calculator = new Calculator();
+        assertThat( calculator.calculation( 2, 4, '*' ) ).isEqualTo( 8 );
+    }
+
+    // Тест для деления
+    @Test
+    void divisionExpression() {
+        Calculator calculator = new Calculator();
+        assertThat( calculator.calculation( 8, 2, '/' ) ).isEqualTo( 4 );
+    }
+
+    // Тест если в метод передан не правильный оператор
+    @Test
+    void expectedIllegalStateExceptionOnInvalidOperatorSymbol() {
+        Calculator calculator = new Calculator();
+        assertThatThrownBy( () -> calculator.calculation( 8, 4, '_' ) )
+                .isInstanceOf( IllegalStateException.class );
+    }
+
+    // Тест проверки, что метод отрабатывает корректно, если в метод передано число
+    @Test
+    void getOperandCompletesCorrectlyWithNumbers() {
+        String testedValue = "9";
+        ByteArrayInputStream in = new ByteArrayInputStream(  testedValue.getBytes());
+        InputStream inputStream = System.in; // сохраним ссылку на ввод с клавиатуры
+        System.setIn( in ); // подмена ввода
+
+        Calculator.getOperand();
+
+        System.out.println(testedValue);
+
+        System.setIn( inputStream );  // подмена ссылки на ввод с клавиатуры обратно
+
+    }
+
+    // Тест проверки, если в метод пришёл символ буквы
+    @Test
+    void getOperandCompletesCorrectlyWithNotNumbers() {
+        String testedValue = "k";
+        ByteArrayInputStream in = new ByteArrayInputStream( testedValue.getBytes() );
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        InputStream inputStream = System.in;
+        System.setIn( in );
+        System.setOut( new PrintStream( out ) );
+
+        assertThatThrownBy( () -> Calculator.getOperand() ).isInstanceOf( IllegalStateException.class )
+                .describedAs( "Ошибка в вводимых данных" );
+
+        System.setIn( inputStream );
+        System.setOut( null );
+    }
+}
