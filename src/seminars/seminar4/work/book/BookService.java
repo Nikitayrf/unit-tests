7fd2a8d5df64a3d01e8876fbfd3d500918fa830b
nikita <samaraphone@yandex.ru>
2023-10-25 20:37:13 +0300

    add homework-4

--- src/seminars/seminar4/work/book/BookService.java
+++ src/seminars/seminar4/work/book/BookService.java
@@ -3,6 +3,9 @@ package seminars.seminar4.work.book;
 import java.util.List;
 
 public class BookService {
+    // Логика BookService завязана на логику BookRepository, т.е. есть внутренняя зависимость.
+    // Что бы отвязаться от внутренних зависимостей необходимо создать mock-объект для BookService
+    // т.е. сэмулировать работу BookService
     private BookRepository bookRepository;
 
     public BookService(BookRepository bookRepository) {
